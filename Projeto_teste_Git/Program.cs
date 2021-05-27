1569eaa3dc8f7ae2c0643a15e417214e9c4b03d0
Victor Garcia <victorhbg84@yahoo.com.br>
2021-05-27 18:56:10 -0300

    Projeto iniciado. Hello World

---
+++ Projeto_teste_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_teste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
